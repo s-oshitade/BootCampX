001a5a8627a8a26dd505619cd8b4889f95e662d9
Seun Oshitade <seun.oshitade@gmail.com>
2022-04-09 12:29:36 +0000

    Add BootcampX Queries 4

---
+++ 4_queries/student_assistance_requests.sql
@@ -0,0 +1,10 @@
+SELECT
+  count(assistance_requests.*) AS total_assistances,
+  students.name AS name
+FROM
+  assistance_requests
+  JOIN students ON students.id = student_id
+WHERE
+  students.name = 'Elliot Dickinson'
+GROUP BY
+  name;
