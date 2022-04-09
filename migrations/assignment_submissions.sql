15cded1f88ad9486cedda292eb25f5ea9688105c
Seun Oshitade <seun.oshitade@gmail.com>
2022-04-09 12:33:29 +0000

    Fix typo

--- migrations/assignment_submissions.sql
+++ migrations/assignment_submissions.sql
@@ -11,6 +11,6 @@ CREATE TABLE assignment_submissions (
   id SERIAL PRIMARY KEY NOT NULL,
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
-  duratioN INTEGER,
+  duration INTEGER,
   submission_date DATE
 );
